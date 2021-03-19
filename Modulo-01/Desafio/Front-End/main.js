c300d9b3b6d8e08128d2ee71dda9a96238fed89f
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2021-03-18 22:05:10 -0300

    Final modulo 1

--- Modulo-01/Desafio/Front-End/main.js
+++ Modulo-01/Desafio/Front-End/main.js
@@ -1,5 +1,7 @@
 let employees = [];
 let roles = [];
+let cargosSelecionados = [];
+let funcionariosFiltrados = [];
 
 const criterioOrdenacao = [
   'Nome (ascendente)',
@@ -10,7 +12,7 @@ const criterioOrdenacao = [
 
 const divMain = document.getElementById('divMain');
 const componenteOrdenacao = document.getElementById('criterioOrdenacao');
-const componenteTabela = document.getElementById('componenteTabela');
+const componenteTabela = document.getElementById('dadosTabela');
 const componenteGroupBoxRoles = document.getElementById(
   'componenteGroupBoxRoles'
 );
@@ -20,10 +22,68 @@ async function init() {
 
   preencheCriteioBusca();
   preencheListaRoles();
-  preencheTabela(employees);
+  funcionariosFiltrados = employees;
+  preencheTabela();
+
+  componenteOrdenacao.addEventListener('change', eventoComponenteOrdenacao);
 }
 
 init();
+
+function obterCargos(evento) {
+  const cargo = evento.target;
+
+  if (cargosSelecionados.indexOf(cargo.value) > -1 && cargo.checked == false) {
+    const indice = cargosSelecionados.indexOf(cargo.value);
+
+    cargosSelecionados.splice(indice, 1);
+  } else {
+    cargosSelecionados.push(cargo.value);
+  }
+
+  aplicarFiltroConformeCargosSelecionados();
+}
+
+function aplicarFiltroConformeCargosSelecionados() {
+  funcionariosFiltrados = employees;
+
+  if (cargosSelecionados.length > 0) {
+    funcionariosFiltrados = funcionariosFiltrados.filter((funcionario) =>
+      cargosSelecionados.includes(funcionario.role_id.toString())
+    );
+    eventoComponenteOrdenacao();
+  } else {
+    eventoComponenteOrdenacao();
+  }
+}
+
+function eventoComponenteOrdenacao() {
+  ordena();
+
+  preencheTabela();
+}
+
+function ordena() {
+  const ordenacao = componenteOrdenacao.value;
+
+  if (ordenacao == 0) {
+    funcionariosFiltrados.sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
+  } else if (ordenacao == 1) {
+    funcionariosFiltrados.sort(function (a, b) {
+      return b.name.localeCompare(a.name);
+    });
+  } else if (ordenacao == 2) {
+    funcionariosFiltrados.sort(function (a, b) {
+      return a.salary - b.salary;
+    });
+  } else {
+    funcionariosFiltrados.sort(function (a, b) {
+      return b.salary - a.salary;
+    });
+  }
+}
 
 function preencheCriteioBusca() {
   let identificadorCriterio = 0;
@@ -44,21 +104,30 @@ async function preencheListaRoles() {
   for (const role of roles) {
     const checkBox = document.createElement('input');
     const label = document.createElement('label');
+    const div = document.createElement('div');
 
+    div.id = 'divCheckBox';
     checkBox.type = 'checkbox';
     checkBox.value = role.id;
     checkBox.name = 'role';
 
+    checkBox.addEventListener('click', obterCargos);
+
     label.textContent = role.name;
     label.for = checkBox.name;
 
-    componenteGroupBoxRoles.appendChild(checkBox);
-    componenteGroupBoxRoles.appendChild(label);
+    div.appendChild(checkBox);
+    div.appendChild(label);
+    componenteGroupBoxRoles.appendChild(div);
   }
 }
 
-async function preencheTabela(employeesfilter) {
-  for (const employe of employeesfilter) {
+async function preencheTabela() {
+  componenteTabela.innerHTML = '';
+
+  ordena();
+
+  for (const employe of funcionariosFiltrados) {
     const linha = document.createElement('tr');
     const conteudoLinhaId = document.createElement('td');
     const conteudoLinhaName = document.createElement('td');
--- Modulo-01/JavaScript/a04.1-desafio-crud/solucao/main.js
+++ Modulo-01/JavaScript/a04.1-desafio-crud/solucao/main.js
@@ -1,48 +1,48 @@
 let employees = [];
 let roles = [];
 let selectedItem;
-const listEl = document.querySelector("ul");
-const formEl = document.querySelector("form");
-const bdelete = document.getElementById("bdelete");
-const bcancel = document.getElementById("bcancel");
-const bsubmit = document.getElementById("bsubmit");
+const listEl = document.querySelector('ul');
+const formEl = document.querySelector('form');
+const bdelete = document.getElementById('bdelete');
+const bcancel = document.getElementById('bcancel');
+const bsubmit = document.getElementById('bsubmit');
 
 async function init() {
   [employees, roles] = await Promise.all([listEmployees(), listRoles()]);
   renderRoles();
   renderData();
   clearSelection();
-  bcancel.addEventListener("click", clearSelection);
-  formEl.addEventListener("submit", onSubmit);
-  bdelete.addEventListener("click", onDelete);
+  bcancel.addEventListener('click', clearSelection);
+  formEl.addEventListener('submit', onSubmit);
+  bdelete.addEventListener('click', onDelete);
 }
 init();
 
 function selectItem(employee, li) {
   clearSelection();
   selectedItem = employee;
-  li.classList.add("selected");
+  li.classList.add('selected');
   formEl.name.value = employee.name;
   formEl.salary.valueAsNumber = employee.salary;
   formEl.role_id.value = employee.role_id;
-  bdelete.style.display = "inline";
-  bcancel.style.display = "inline";
-  bsubmit.textContent = "Update";
+  bdelete.style.display = 'inline';
+  bcancel.style.display = 'inline';
+  bsubmit.textContent = 'Update';
 }
 
 function clearSelection() {
   clearError();
   selectedItem = undefined;
-  const li = listEl.querySelector(".selected");
+  const li = listEl.querySelector('.selected');
   if (li) {
-    li.classList.remove("selected");
+    li.classList.remove('selected');
   }
-  formEl.name.value = "";
-  formEl.salary.value = "";
-  formEl.role_id.value = "";
-  bdelete.style.display = "none";
-  bcancel.style.display = "none";
-  bsubmit.textContent = "Create";
+  formEl.name.value = '';
+  formEl.salary.value = '';
+  formEl.role_id.value = '';
+  bdelete.style.display = 'none';
+  bcancel.style.display = 'none';
+  bsubmit.textContent = 'Create';
 }
 
 async function onSubmit(evt) {
@@ -53,7 +53,7 @@ async function onSubmit(evt) {
     role_id: +formEl.role_id.value,
   };
   if (!employeeData.name || !employeeData.salary || !employeeData.role_id) {
-    showError("You must fill all form fields.");
+    showError('You must fill all form fields.');
   } else {
     if (selectedItem) {
       const updatedItem = await updateEmployee(selectedItem.id, employeeData);
@@ -82,24 +82,24 @@ async function onDelete() {
 }
 
 function renderData() {
-  listEl.innerHTML = "";
+  listEl.innerHTML = '';
   for (const employee of employees) {
     let role = roles.find((role) => role.id == employee.role_id);
-    const li = document.createElement("li");
-    const divName = document.createElement("div");
+    const li = document.createElement('li');
+    const divName = document.createElement('div');
     divName.textContent = employee.name;
-    const divRole = document.createElement("div");
+    const divRole = document.createElement('div');
     divRole.textContent = role.name;
     li.appendChild(divName);
     li.appendChild(divRole);
     listEl.appendChild(li);
-    li.addEventListener("click", () => selectItem(employee, li));
+    li.addEventListener('click', () => selectItem(employee, li));
   }
 }
 
 function renderRoles() {
   for (const role of roles) {
-    const option = document.createElement("option");
+    const option = document.createElement('option');
     option.textContent = role.name;
     option.value = role.id;
     formEl.role_id.appendChild(option);
@@ -107,12 +107,12 @@ function renderRoles() {
 }
 
 function showError(message, error) {
-  document.getElementById("errors").textContent = message;
+  document.getElementById('errors').textContent = message;
   if (error) {
     console.error(error);
   }
 }
 
 function clearError() {
-  document.getElementById("errors").textContent = "";
+  document.getElementById('errors').textContent = '';
 }
